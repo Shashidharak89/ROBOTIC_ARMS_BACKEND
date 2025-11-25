50a742d09924ad9c2464f5f4ad3d4134004428e7
Shashidharak89 <shashidharak334@gmail.com>
2025-11-25 09:24:18 +0530

    First commit, Initial setup

---
+++ src/main/java/com/arms/robotic_arms/RoboticArmsApplication.java
@@ -0,0 +1,13 @@
+package com.arms.robotic_arms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboticArmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboticArmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arms/robotic_arms/RoboticArmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arms.robotic_arms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboticArmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
